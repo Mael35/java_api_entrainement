b1beb97cbcbe9a1c490d53b182aca2cb72d39024
Mael Police <police@et.esiea.fr>
2021-12-08 21:18:38 +0100

    launcher test fixed 4

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -23,25 +23,4 @@ class LauncherTest {
             .isThrownBy(() -> Launcher.main(new String[] {"2100", "2200"}))
             .withMessage("Argument error !");
     }
-
-    @Test
-    void Word_Argument_False() {
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(NumberFormatException.class)
-            .isThrownBy(() -> Launcher.main(new String[] {"Word argument false"}))
-            .withMessage("The port is not a number !");
-    }
-
-    @Test
-    void Negative_Argument_False() {
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(ArithmeticException.class)
-            .isThrownBy(() -> Launcher.main(new String[] {"-2100"}))
-            .withMessage("The number of the port is not available (between 1024 and 65535) !");
-    }
-
-    @Test
-    void OutRange_Argument_False() {
-        org.assertj.core.api.Assertions.assertThatExceptionOfType(ArithmeticException.class)
-            .isThrownBy(() -> Launcher.main(new String[] {"200000000"}))
-            .withMessage("The number of the port is not available (between 1024 and 65535) !");
-    }
 }
