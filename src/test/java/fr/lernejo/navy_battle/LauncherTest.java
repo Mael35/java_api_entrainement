9d8f77730147023b88fd5245fa62e035dea5f487
Mael Police <police@et.esiea.fr>
2021-12-18 21:03:09 +0100

    Part 4 with TESTS V2

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -2,6 +2,11 @@ package fr.lernejo.navy_battle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class LauncherTest {
 
     @Test
@@ -16,13 +21,29 @@ class LauncherTest {
             .isThrownBy(() -> Launcher.main(new String[] {}))
             .withMessage("Argument error !");
     }
-    /*
+
+    boolean Check_not_equals(String port1, String port2){
+        return !Objects.equals(port1, port2)  ;
+    }
+
+    @Test
+    void Not_Same_Argument_True() {
+        String port1 = "2100";
+        String port2 = "2200";
+        assertTrue(Check_not_equals(port1, port2));
+    }
+
+    @Test
+    void Same_Argument_False() {
+        String port1 = "2300";
+        String port2 = "2300";
+        assertFalse(Check_not_equals(port1, port2));
+    }
+
     @Test
     void TooMany_Argument_False() {
         org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> Launcher.main(new String[] {"2100", "2200"}))
+            .isThrownBy(() -> Launcher.main(new String[] {"2400", "2500", "2600"}))
             .withMessage("Argument error !");
     }
-
-    */
 }
