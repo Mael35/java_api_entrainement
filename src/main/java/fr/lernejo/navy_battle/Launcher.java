2646bad3ac412982e6a4c5274f5d1095357b8717
Mael Police <police@et.esiea.fr>
2021-12-18 12:50:16 +0100

    Part 3 with TESTS

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class Launcher {
     public static void main(String[] args) throws IOException {
+
         if (args.length == 1) {
             Http_Server http_server = new Http_Server(args[0]);
             http_server.createServer();
